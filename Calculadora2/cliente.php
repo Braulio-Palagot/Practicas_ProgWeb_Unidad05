5364942332fdea1bf992b2eef1e2f20b549f12f6
JafetMijangos <jafetmijangos@hotmail.com>
2022-05-18 11:48:14 -0500

    Proyecto

--- Calculadora2/cliente.php
+++ Calculadora2/cliente.php
@@ -14,10 +14,45 @@
 if (isset($_GET["action"]) && $_GET["action"] == "suma")
 {
     $calculadora = file_get_contents(
-        'http://calculadora2.test/servicio.php?action=suma'.$_GET);
-    $calculadora
-}
-
-
+        'http://localhost/practicas_progweb_unidad05/Calculadora2/servicio.php?action=suma&a='.$_GET["a"].'&b='.$_GET["b"]);
+    $calculadora = json_decode($calculadora,true);
 ?>
 
+<h1>SUMA</h1>
+<table>
+    <tr>
+        <td>Primer numero : </td><td> <?php echo $calculadora["a"] ?></td>
+    </tr>
+    <tr>
+        <td>Segundo numero : </td><td> <?php echo $calculadora["b"] ?></td>
+    </tr>
+    <tr>
+        <td>Total : </td><td><h3> <?php echo $calculadora["total"] ?></h3></td>
+    </tr>
+    </table>
+    <?php
+}
+
+if (isset($_GET["action"]) && $_GET["action"] == "multiplicacion")
+{
+    $calculadora = file_get_contents(
+        'http://localhost/practicas_progweb_unidad05/Calculadora2/servicio.php?action=suma&a='.$_GET["a"].'&b='.$_GET["b"]);
+    $calculadora = json_decode($calculadora,true);
+?>
+
+<h1>MULTIPLICACI0N</h1>
+<table>
+    <tr>
+        <td>Primer numero : </td><td> <?php echo $calculadora["a"] ?></td>
+    </tr>
+    <tr>
+        <td>Segundo numero : </td><td> <?php echo $calculadora["b"] ?></td>
+    </tr>
+    <tr>
+        <td>Total : </td><td><h3> <?php echo $calculadora["total"] ?></h3></td>
+    </tr>
+    </table>
+    <?php
+}
+
+?>
