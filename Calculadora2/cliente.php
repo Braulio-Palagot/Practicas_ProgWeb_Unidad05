798d6df7b9123b9a305c6be721e93c8722c8da3f
Brau_Palagot <bdpalagot@gmail.com>
2022-05-20 15:15:05 -0500

    UPDATE Calculadora 2
    Cambios Realizados:
        1) Se agregaron las operaciones de Resta y División a la Calculadora 2 con Servicio Web REST.

--- Calculadora2/cliente.php
+++ Calculadora2/cliente.php
@@ -1,56 +1,110 @@
 <H1>Calculadora</H1>
 <form action="cliente.php" method="get">
-    Numero 1: <input type="text" name="a"><br> 
+    Numero 1: <input type="text" name="a"><br>
     Numero 2: <input type="text" name="b"><br>
     Operacion: <select name="action">
         <option value="suma">Sumar</option>
+        <option value="resta">Restar</option>
         <option value="multiplicacion">Multiplicar</option>
-</select><br><br>
-<input type ="submit" value="Calcular">
+        <option value="division">Dividir</option>
+    </select><br><br>
+    <input type="submit" value="Calcular">
 </form>
 
 
 <?php
-if (isset($_GET["action"]) && $_GET["action"] == "suma")
-{
+if (isset($_GET["action"]) && $_GET["action"] == "suma") {
     $calculadora = file_get_contents(
-        'http://localhost/practicas_progweb_unidad05/Calculadora2/servicio.php?action=suma&a='.$_GET["a"].'&b='.$_GET["b"]);
-    $calculadora = json_decode($calculadora,true);
-?>
+        'http://localhost/practicas_progweb_unidad05/Calculadora2/servicio.php?action=suma&a=' . $_GET["a"] . '&b=' . $_GET["b"]);
+    $calculadora = json_decode($calculadora, true);
+    ?>
 
-<h1>SUMA</h1>
-<table>
-    <tr>
-        <td>Primer numero : </td><td> <?php echo $calculadora["a"] ?></td>
-    </tr>
-    <tr>
-        <td>Segundo numero : </td><td> <?php echo $calculadora["b"] ?></td>
-    </tr>
-    <tr>
-        <td>Total : </td><td><h3> <?php echo $calculadora["total"] ?></h3></td>
-    </tr>
+    <h1>SUMA</h1>
+    <table>
+        <tr>
+            <td>Primer numero :</td>
+            <td> <?php echo $calculadora["a"] ?></td>
+        </tr>
+        <tr>
+            <td>Segundo numero :</td>
+            <td> <?php echo $calculadora["b"] ?></td>
+        </tr>
+        <tr>
+            <td>Total :</td>
+            <td><h3> <?php echo $calculadora["total"] ?></h3></td>
+        </tr>
     </table>
     <?php
 }
 
-if (isset($_GET["action"]) && $_GET["action"] == "multiplicacion")
-{
+if (isset($_GET["action"]) && $_GET["action"] == "multiplicacion") {
     $calculadora = file_get_contents(
-        'http://localhost/practicas_progweb_unidad05/Calculadora2/servicio.php?action=multiplicacion&a='.$_GET["a"].'&b='.$_GET["b"]);
-    $calculadora = json_decode($calculadora,true);
-?>
+        'http://localhost/practicas_progweb_unidad05/Calculadora2/servicio.php?action=multiplicacion&a=' . $_GET["a"] . '&b=' . $_GET["b"]);
+    $calculadora = json_decode($calculadora, true);
+    ?>
 
-<h1>MULTIPLICACIÓN</h1>
-<table>
-    <tr>
-        <td>Primer numero : </td><td> <?php echo $calculadora["a"] ?></td>
-    </tr>
-    <tr>
-        <td>Segundo numero : </td><td> <?php echo $calculadora["b"] ?></td>
-    </tr>
-    <tr>
-        <td>Total : </td><td><h3> <?php echo $calculadora["total"] ?></h3></td>
-    </tr>
+    <h1>MULTIPLICACIÓN</h1>
+    <table>
+        <tr>
+            <td>Primer numero :</td>
+            <td> <?php echo $calculadora["a"] ?></td>
+        </tr>
+        <tr>
+            <td>Segundo numero :</td>
+            <td> <?php echo $calculadora["b"] ?></td>
+        </tr>
+        <tr>
+            <td>Total :</td>
+            <td><h3> <?php echo $calculadora["total"] ?></h3></td>
+        </tr>
+    </table>
+    <?php
+}
+
+if (isset($_GET["action"]) && $_GET["action"] == "resta") {
+    $calculadora = file_get_contents(
+        'http://localhost/practicas_progweb_unidad05/Calculadora2/servicio.php?action=resta&a=' . $_GET["a"] . '&b=' . $_GET["b"]);
+    $calculadora = json_decode($calculadora, true);
+    ?>
+
+    <h1>RESTA</h1>
+    <table>
+        <tr>
+            <td>Primer numero :</td>
+            <td> <?php echo $calculadora["a"] ?></td>
+        </tr>
+        <tr>
+            <td>Segundo numero :</td>
+            <td> <?php echo $calculadora["b"] ?></td>
+        </tr>
+        <tr>
+            <td>Total :</td>
+            <td><h3> <?php echo $calculadora["total"] ?></h3></td>
+        </tr>
+    </table>
+    <?php
+}
+
+if (isset($_GET["action"]) && $_GET["action"] == "division") {
+    $calculadora = file_get_contents(
+        'http://localhost/practicas_progweb_unidad05/Calculadora2/servicio.php?action=division&a=' . $_GET["a"] . '&b=' . $_GET["b"]);
+    $calculadora = json_decode($calculadora, true);
+    ?>
+
+    <h1>DIVISIÓN</h1>
+    <table>
+        <tr>
+            <td>Primer numero :</td>
+            <td> <?php echo $calculadora["a"] ?></td>
+        </tr>
+        <tr>
+            <td>Segundo numero :</td>
+            <td> <?php echo $calculadora["b"] ?></td>
+        </tr>
+        <tr>
+            <td>Total :</td>
+            <td><h3> <?php echo $calculadora["total"] ?></h3></td>
+        </tr>
     </table>
     <?php
 }
--- Calculadora2/servicio.php
+++ Calculadora2/servicio.php
@@ -1,5 +1,5 @@
 <?php
-$possible_url = array("suma", "multiplicacion");
+$possible_url = array("suma", "multiplicacion", "resta", "division");
 $value = "An error has occured";
 
 if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url)) {
@@ -9,6 +9,12 @@ if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url)) {
             break;
         case "multiplicacion":
             $value = multiplicacion($_GET["a"], $_GET["b"]);
+            break;
+        case "division":
+            $value = division($_GET["a"], $_GET["b"]);
+            break;
+        case "resta":
+            $value = resta($_GET["a"], $_GET["b"]);
             break;
     }
 }
@@ -25,6 +31,20 @@ function multiplicacion($a, $b)
 {
     $total = array();
     $total = array("a" => "$a", "b" => "$b", "total" => $a * $b);
+    return $total;
+}
+
+function division($a, $b)
+{
+    $total = array();
+    $total = array("a" => "$a", "b" => "$b", "total" => $a / $b);
+    return $total;
+}
+
+function resta($a, $b)
+{
+    $total = array();
+    $total = array("a" => "$a", "b" => "$b", "total" => $a - $b);
     return $total;
 }
 ?>
