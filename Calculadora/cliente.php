b637b0aa3532db02bc737c57e3ac5e12318569d1
Brau_Palagot <bdpalagot@gmail.com>
2022-05-20 16:27:18 -0500

    UPDATE Calculadora 1
    Cambios Realizados:
        1) Se corrigió el error por el cual no funcionaba el servicio web nusoap que se debía a la versión de PHP, no se podía utilizar *service($HTTP_RAW_POST_DATA)* sino que debía usarse *service(file_get_contents("php://input"))*.
        2) Se realizaron las otras 3 operaciones matemáticas disponibles.

--- Calculadora/cliente.php
+++ Calculadora/cliente.php
@@ -1,10 +1,34 @@
 <?php
 require_once('lib/nusoap.php');
-$cliente = new nusoap_client('http://localhost/Calculadora/servicio.php');
+$cliente = new nusoap_client('http://localhost/practicas_progweb_unidad05/Calculadora/servicio.php');
+
+// Multiplicación:
 $resultado = $cliente->call('calculadora',
-  array( 'x' => '3',
-         'y' => 4,
-         'operacion' => 'muliplica' ));
- 
-  echo implode($resultado)
+    array('x' => '3',
+        'y' => 4,
+        'operacion' => 'multiplica'));
+echo "<b>Resultado (3*4):</b> " . $resultado . "<br><br>";
+
+// División:
+$resultado = $cliente->call('calculadora',
+    array('x' => '3',
+        'y' => 4,
+        'operacion' => 'divide'));
+echo "<b>Resultado (3/4):</b> " . $resultado . "<br><br>";
+
+// Suma:
+$resultado = $cliente->call('calculadora',
+    array('x' => '3',
+        'y' => 4,
+        'operacion' => 'suma'));
+echo "<b>Resultado (3+4):</b> " . $resultado . "<br><br>";
+
+// Resta:
+$resultado = $cliente->call('calculadora',
+    array('x' => '3',
+        'y' => 4,
+        'operacion' => 'resta'));
+echo "<b>Resultado (3-4):</b> " . $resultado;
+
+//phpinfo();
 ?>
--- Calculadora/servicio.php
+++ Calculadora/servicio.php
@@ -6,5 +6,8 @@ $server = new nusoap_server();
 $server ->register('calculadora');
 
 $HTTP_RAW_POST_DATA = isset ($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
-$server->service($HTTP_RAW_POST_DATA);
+//Por la versión de PHP, no funciona el service($HTTP_RAW_POST_DATA).
+//$server->service($HTTP_RAW_POST_DATA);
+//Por esta razón, se utiliza otra forma de llamar al servicio SOAP:
+@$server->service(file_get_contents("php://input"));
 ?>
