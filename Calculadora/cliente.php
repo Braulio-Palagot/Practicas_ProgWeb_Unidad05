1e8aaab45a3bc21e92765ef60e7e6bd65a83c235
Brau_Palagot <bdpalagot@gmail.com>
2022-05-20 16:57:03 -0500

    UPDATE Calculadora 1
    Cambios Realizados:
        1) Se utilizó un formulario en el cliente para que el usuario pueda introducir los valores y la operación a realizar con dichos valores.

--- Calculadora/cliente.php
+++ Calculadora/cliente.php
@@ -1,34 +1,52 @@
 <?php
 require_once('lib/nusoap.php');
 $cliente = new nusoap_client('http://localhost/practicas_progweb_unidad05/Calculadora/servicio.php');
+$x = isset($_POST["x"])?$_POST["x"]:"";
+$y = isset($_POST["y"])?$_POST["y"]:"";
+$operacion = isset($_POST["operacion"])?$_POST["operacion"]:"";
+?>
 
-// Multiplicación:
-$resultado = $cliente->call('calculadora',
-    array('x' => '3',
-        'y' => 4,
-        'operacion' => 'multiplica'));
-echo "<b>Resultado (3*4):</b> " . $resultado . "<br><br>";
+    <form action="cliente.php" method="post">
+        Numero 1: <input type="text" name="x"><br>
+        Numero 2: <input type="text" name="y"><br>
+        Operacion: <select name="operacion">
+            <option value="suma">Sumar</option>
+            <option value="resta">Restar</option>
+            <option value="multiplica">Multiplicar</option>
+            <option value="divide">Dividir</option>
+        </select><br><br>
+        <input type="submit" value="Calcular">
+    </form>
 
-// División:
-$resultado = $cliente->call('calculadora',
-    array('x' => '3',
-        'y' => 4,
-        'operacion' => 'divide'));
-echo "<b>Resultado (3/4):</b> " . $resultado . "<br><br>";
-
-// Suma:
-$resultado = $cliente->call('calculadora',
-    array('x' => '3',
-        'y' => 4,
-        'operacion' => 'suma'));
-echo "<b>Resultado (3+4):</b> " . $resultado . "<br><br>";
-
-// Resta:
-$resultado = $cliente->call('calculadora',
-    array('x' => '3',
-        'y' => 4,
-        'operacion' => 'resta'));
-echo "<b>Resultado (3-4):</b> " . $resultado;
-
-//phpinfo();
+<?php
+switch ($operacion) {
+    case "multiplica":
+        $resultado = $cliente->call('calculadora',
+            array('x' => $x,
+                'y' => $y,
+                'operacion' => $operacion));
+        echo "<b>Resultado ($x*$y):</b> " . $resultado . "<br><br>";
+        break;
+    case "divide":
+        $resultado = $cliente->call('calculadora',
+            array('x' => $x,
+                'y' => $y,
+                'operacion' => $operacion));
+        echo "<b>Resultado ($x/$y):</b> " . $resultado . "<br><br>";
+        break;
+    case "suma":
+        $resultado = $cliente->call('calculadora',
+            array('x' => $x,
+                'y' => $y,
+                'operacion' => $operacion));
+        echo "<b>Resultado ($x+$y):</b> " . $resultado . "<br><br>";
+        break;
+    case "resta":
+        $resultado = $cliente->call('calculadora',
+            array('x' => $x,
+                'y' => $y,
+                'operacion' => $operacion));
+        echo "<b>Resultado ($x-$y):</b> " . $resultado;
+        break;
+}
 ?>
